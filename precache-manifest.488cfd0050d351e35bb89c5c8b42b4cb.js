59716efa32ac967ee93450bd31166382ecd6d103
basilrao <basilrao123@gmai.com>
2020-08-03 14:25:24 -0700

    Updates

---
+++ precache-manifest.488cfd0050d351e35bb89c5c8b42b4cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beab0743d860d418fdeb3b4a865af89f",
+    "url": "/Shoestore/index.html"
+  },
+  {
+    "revision": "e10b51e5631b4173b292",
+    "url": "/Shoestore/static/css/main.607aede9.chunk.css"
+  },
+  {
+    "revision": "4732f7652262bd9f9399",
+    "url": "/Shoestore/static/js/2.46d27998.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Shoestore/static/js/2.46d27998.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e10b51e5631b4173b292",
+    "url": "/Shoestore/static/js/main.07ee03e1.chunk.js"
+  },
+  {
+    "revision": "fb2e34fb77daaffd3171",
+    "url": "/Shoestore/static/js/runtime-main.e2efae3a.js"
+  }
+]);
